20b8e9760cba8b50f1c2c0620f8e346ad0af16a9
DanielMVP7 <dan6mourex@gmail.com>
2024-04-14 22:33:21 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+function cafe1(imagem) {
+    imagem.src = "https://www.publicdomainpictures.net/pictures/490000/nahled/koffie-kopje-koffie-png.png"
+}
+function cafe2(imagem) {
+    imagem.src = "https://www.imagensempng.com.br/wp-content/uploads/2021/09/06-9.png"
+}
+function cafe3(imagem) {
+    imagem.src = "https://static.vecteezy.com/system/resources/previews/023/438/448/non_2x/espresso-coffee-cutout-free-png.png"
+}
+function cafe4(imagem) {
+    imagem.src = "https://www.pngall.com/wp-content/uploads/3/Espresso-PNG-Free-Image.png"
+}
+function cafe5(imagem) {
+    imagem.src = "https://americancookiesbrasil.com.br/wp-content/uploads/2022/06/mocha.png"
+}
+function cafe6(imagem) {
+    imagem.src = "https://coffeeproject.com.ph/cdn/shop/products/mocha.png?v=1625706554"
+}
+function cafe7(imagem) {
+    imagem.src = "https://www.nescafe.com/pt/sites/default/files/2023-08/Nes_Web3_Article_Header_Macchiato_1448x1240.png"
+}
+function cafe8(imagem) {
+    imagem.src = "https://media.discordapp.net/attachments/843168555360583711/1229233961054634074/png-clipart-latte-macchiato-coffee-cafe-caffe-macchiato-coffee-latte-removebg-preview.png?ex=662ef056&is=661c7b56&hm=24983f4ae8483a871909fd580c32078e17cda646651de95d78a17a1a29629702&=&format=webp&quality=lossless"
+}
